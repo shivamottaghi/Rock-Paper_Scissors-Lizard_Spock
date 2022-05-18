c5cfe30ff47c7f2246dfcd42d17e2863320faa35
shivamtg <shivamtg1376@gmail.com>
2022-05-18 16:50:22 +0200

    add array with all the possible ways that user wins

--- script.js
+++ script.js
@@ -4,19 +4,56 @@
     let possibilities = [
         {
             'user' : '0',
-            'computer': '0',
-            'winner' : 'Tie'
+            'computer': '2',
         },
-    ]
+        {
+            'user' : '0',
+            'computer': '3',
+        },
+        {
+            'user' : '1',
+            'computer': '0',
+        },
+        {
+            'user' : '1',
+            'computer': '4',
+        },
+        {
+            'user' : '2',
+            'computer': '3',
+        },
+        {
+            'user' : '2',
+            'computer': '1',
+        },
+        {
+            'user' : '3',
+            'computer': '1',
+        },
+        {
+            'user' : '3',
+            'computer': '4',
+        },
+        {
+            'user' : '4',
+            'computer': '0',
+        },
+        {
+            'user' : '4',
+            'computer': '2',
+        },
+    ];
+    let
     let userChoiceBtns = document.querySelectorAll('.user-choice');
     userChoiceBtns.forEach(btn => {
         btn.addEventListener('click', ()=> {
             let usersChoice = btn.value;
             let computerChoice = Math.floor(Math.random()*5);
+            console.log(computerChoice)
             if (usersChoice == computerChoice){
                 console.log('Tie');
             }
-
+            let findReturn =
         })
     })
 
